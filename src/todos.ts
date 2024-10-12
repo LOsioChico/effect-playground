dde23efe6f2efebf4c4eb4dbdc815102798df570
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-10-12 03:03:44 -0500

    feat: update todos script with BASE_URL config

--- src/todos.ts
+++ src/todos.ts
@@ -1,100 +1,51 @@
-const API_URL = "https://jsonplaceholder.typicode.com/todos";
-
-interface Todo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-// Problems with this imperative approach:
-// 1. Error handling is mixed with the main logic, making it harder to reason about.
-// 2. The function doesn't handle different types of errors distinctly.
-// 3. It doesn't provide a way to cancel the operation if needed.
-// 4. Lacks proper resource management (e.g., closing connections).
-// 5. Doesn't handle potential JSON parsing errors separately.
-// 6. Returns an empty array on error, which might mask issues.
-// 7. Doesn't allow for easy retries or backoff strategies.
-const getTodosImperative = async (): Promise<Todo[] | undefined> => {
-  try {
-    const response = await fetch(API_URL);
-    if (!response.ok)
-      throw new Error(
-        `[getTodosImperative] HTTP error! status: ${response.status}`
-      );
-
-    const todos = await response.json();
-    return todos;
-  } catch (error) {
-    console.error(`[getTodosImperative] Error fetching todos: ${error}`);
-  }
-};
-
-const todos = await getTodosImperative();
-if (todos) console.log(`[getTodosImperative] Todos: ${todos.length}`);
-
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-
-import { Data, Effect } from "effect";
 import { Schema } from "@effect/schema";
+import { Config, Data, Effect } from "effect";
 
-const ResponseSchema = Schema.Array(
-  Schema.Struct({
-    userId: Schema.Int,
-    id: Schema.Int,
-    title: Schema.String,
-    completed: Schema.Boolean,
+class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
+  id: Schema.Number,
+  order: Schema.Number,
+  name: Schema.String,
+  height: Schema.Number,
+  weight: Schema.Number,
+}) {}
+
+class FetchError extends Data.TaggedError("FetchError")<{}> {}
+class JsonError extends Data.TaggedError("JsonError")<{}> {}
+
+const config = Config.string("BASE_URL");
+
+const fetchRequest = (baseUrl: string) =>
+  Effect.tryPromise({
+    try: () => fetch(`${baseUrl}/api/v2/pokemon/charizard/`),
+    catch: () => new FetchError(),
+  });
+
+const jsonResponse = (response: Response) =>
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: () => new JsonError(),
+  });
+
+const decodePokemon = Schema.decodeUnknown(Pokemon);
+
+const program = Effect.gen(function* () {
+  const baseUrl = yield* config;
+  const response = yield* fetchRequest(baseUrl);
+  if (!response.ok) {
+    return yield* new FetchError();
+  }
+
+  const json = yield* jsonResponse(response);
+
+  return yield* decodePokemon(json);
+});
+
+const main = program.pipe(
+  Effect.catchTags({
+    FetchError: () => Effect.succeed("Fetch error"),
+    JsonError: () => Effect.succeed("Json error"),
+    ParseError: () => Effect.succeed("Parse error"),
   })
 );
 
-class RequestError extends Data.TaggedError("RequestError")<{
-  message: string;
-}> {}
-class NotOkError extends Data.TaggedError("NotOkError") {}
-class NotJsonError extends Data.TaggedError("NotJsonError") {}
-class NoElementsError extends Data.TaggedError("NoElementsError") {}
-
-const getTodosEffect = Effect.gen(function* (_) {
-  const response = yield* _(
-    Effect.tryPromise({
-      try: () => fetch(API_URL),
-      catch: (error: unknown) => new RequestError({ message: String(error) }),
-    }),
-    Effect.filterOrElse(
-      (response) => response.ok,
-      () => new NotOkError()
-    )
-  );
-
-  const todos = yield* _(
-    Effect.tryPromise({
-      try: () => response.json(),
-      catch: () => new NotJsonError(),
-    })
-  );
-
-  const parsedTodos = yield* _(
-    Schema.decodeUnknown(ResponseSchema, {
-      exact: true,
-    })(todos),
-    Effect.filterOrElse(
-      (todos) => todos.length > 0,
-      () => new NoElementsError()
-    )
-  );
-
-  return parsedTodos;
-});
-await getTodosEffect.pipe(
-  Effect.catchAll((error) => {
-    console.error(`[getTodosEffect] Error fetching todos: ${error.message}`);
-    return Effect.fail(error);
-  }),
-  Effect.tap((todos) => {
-    console.log(`[getTodosEffect] Todos: ${todos.length}`);
-  }),
-  Effect.ignore,
-  Effect.runPromise
-);
+Effect.runPromise(main).then(console.log);
